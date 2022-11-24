16847d49ac5fac16736aedef571f5ca2b6dd8419
OgabekDev <ogabekdev@gmail.com>
2022-11-24 15:13:01 +0500

    Initial Commit

---
+++ GithubDemo2/GithubDemo2App.swift
@@ -0,0 +1,17 @@
+//
+//  GithubDemo2App.swift
+//  GithubDemo2
+//
+//  Created by Ogabek Matyakubov on 24/11/22.
+//
+
+import SwiftUI
+
+@main
+struct GithubDemo2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
